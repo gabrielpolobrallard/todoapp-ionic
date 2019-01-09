0e6d0257a36c22b05dac463cfd6a912ef63c6463
Gabriel Polo Brallard <polo1@avaya.com>
2019-01-08 21:30:46 -0300

    Removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -16,9 +14,6 @@ import { CompletedPage } from './../pages/completed/completed.component';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     TodoPage,
     CompletedPage
@@ -30,9 +25,6 @@ import { CompletedPage } from './../pages/completed/completed.component';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     TodoPage,
     CompletedPage
